2f5153d6e0527ffcec42a46e76480fa6e239a7b5
Afra <propazaman12@gmail.com>
2023-06-14 23:10:54 +0600

    commented await

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const schoolCollection = client.db("graphicsSchool").collection("school");
     // const reviewCollection = client.db("bistroDb").collection("reviews");
