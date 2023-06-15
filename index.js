d39eabf9e55d372d43a8609f3c9fbf51059d3f76
Afra <propazaman12@gmail.com>
2023-06-15 17:00:34 +0600

    instructor get and patch

--- index.js
+++ index.js
@@ -139,6 +139,34 @@ async function run() {
   
       })
 
+      app.get('/users/instructor/:email', async (req, res) => {
+        const email = req.params.email;
+  
+        if (req.decoded.email !== email) {
+          res.send({ admin: false })
+        }
+  
+        const query = { email: email }
+        const user = await usersCollection.findOne(query);
+        const result = { admin: user?.role === 'admin' }
+        res.send(result);
+      })
+
+      app.patch('/users/instructor/:id', async (req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: 'admin'
+          },
+        };
+  
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+  
+      })
+
 
   
 
